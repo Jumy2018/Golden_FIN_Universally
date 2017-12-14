8cd49c19586997db57db8febbafb0ea56e686739
lijianl <li1284537512@gmail.com>
2017-12-14 23:47:02 +0800

    1. 删除实例文件。 2. 修改数据库连接

--- src/main/java/com/finuniversally/controller/MainController.java
+++ src/main/java/com/finuniversally/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.finuniversally.controller;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,24 +9,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.finuniversally.model.City;
-import com.finuniversally.service.CityService;
+import com.finuniversally.model.Variety;
+import com.finuniversally.service.VarietyService;
 
 @Controller
 public class MainController {
+	//日志
 	private static final Logger log = Logger.getLogger(MainController.class.getName());
 	
 	@Autowired
-	private CityService cityService;
+	private VarietyService varietyService;
 	
 	@RequestMapping(value={"/"},method={RequestMethod.GET})
 	public String head() {
+		List<Variety> varietys = varietyService.getVarietys();
+		for(Variety variety: varietys) {
+			System.out.println(variety.getVarietyName());
+		}
 		return "index";
 	}
 	@RequestMapping(value={"/index"},method={RequestMethod.GET})
 	public String index(Model model) throws Exception{
-		City city = cityService.getCityById(1);
-		model.addAttribute("msg",city.getName());
 		return "index";
 	}
 }
