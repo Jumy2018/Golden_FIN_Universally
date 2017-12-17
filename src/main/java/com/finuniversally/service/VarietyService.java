6de63263dd264e2bb180de0ef01d7ce42a40d539
lijianl <li1284537512@gmail.com>
2017-12-17 14:03:40 +0800

    获取品种数据

---
+++ src/main/java/com/finuniversally/service/VarietyService.java
@@ -0,0 +1,14 @@
+package com.finuniversally.service;
+
+import java.util.List;
+import com.finuniversally.model.Variety;
+
+/**
+ * 品种相关业务
+ * @author riseSun
+ *
+ * 2017年12月14日下午10:20:39
+ */
+public interface VarietyService {
+	public List<Variety> getVarietys();
+}
---
+++ src/main/java/com/finuniversally/service/impl/VarietyServiceImpl.java
@@ -0,0 +1,23 @@
+package com.finuniversally.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.finuniversally.dao.VarietyDao;
+import com.finuniversally.model.Variety;
+import com.finuniversally.service.VarietyService;
+
+@Component
+@Transactional
+public class VarietyServiceImpl implements VarietyService{
+	@Autowired
+	private VarietyDao varietyDao;
+	
+	@Override
+	public List<Variety> getVarietys() {
+		return varietyDao.getVarietys();
+	}
+}
