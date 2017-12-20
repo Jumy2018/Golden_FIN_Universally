8d5849031e2f4355be65dc9d923462a816af8807
kezheny <kezheny@DESKTOP-UACM1Q5>
2017-12-20 23:56:31 +0800

    页面修改

--- src/main/java/com/finuniversally/controller/MainController.java
+++ src/main/java/com/finuniversally/controller/MainController.java
@@ -38,6 +38,6 @@ public class MainController {
 	}
 	@RequestMapping(value={"/index"},method={RequestMethod.GET})
 	public String index() throws Exception{
-		return "index";
+		return "test";
 	}
 }
