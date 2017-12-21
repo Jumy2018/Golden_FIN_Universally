2533b594dcb74ca1a888c181c45b67204a3143c9
lijianl <li1284537512@gmail.com>
2017-12-21 22:05:14 +0800

    解决冲突

--- src/main/java/com/finuniversally/dao/OrderDao.java
+++ src/main/java/com/finuniversally/dao/OrderDao.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public interface OrderDao {
 	
 	@Select("select sum(qty) from ${arg0} t where symbol=#{arg1} and t.cmd=#{arg2,jdbcType=INTEGER} ")
-	public Double getHoldQtys(String table,String variety,Long cmd);
+	public Double getHoldQtys( String table,String variety,Long cmd);
 	
 
 }
